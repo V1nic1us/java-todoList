905c7092f7da5a22db3cc5622bc738c0b61fddc3
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-14 00:37:47 -0400

    feat: add initial test class for todolist application context loading.

---
+++ src/test/java/br/com/v1nic1us/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.v1nic1us.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
